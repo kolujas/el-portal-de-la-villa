b8a4efccaa8d7235f6a3a8d87dd9c7bfd71e2fa1
kolujAs <fernandodeibe@gmail.com>
2019-11-26 16:03:27 -0300

    thank-you page

--- resources/views/correo/gracias.blade.php
+++ resources/views/correo/gracias.blade.php
@@ -14,15 +14,15 @@
 @endsection
 
 @section('main')
-    <div class="jumbotron card card-image d-lg-block m-0 p-5 px-0"
+    <div class="jumbotron card card-image d-lg-block m-0 px-0 col-12"
     style="background: url(/img/recursos/banner-thanks.jpg) no-repeat center center;">
-        <div class="text-white text-center py-5 p-0">
+        <div class="text-white text-center py-3 p-0 gracias-div">
             <div class="py-md-5">
-                <h2 class="card-title h1-responsive p-0 mb-4 mt-lg-4 font-weight-bold text-white">
+                <h2 class="card-title h1-responsive p-0 mb-4 mt-lg-4 font-weight-bold text-dark">
                     <strong>¡Muchas Gracias!</strong>
                 </h2>
-                <p class="mb-4">Te responderemos en la brevedad.</p>
-                <a class="btn btn-lg mt-lg-4" href="/">Regresar al Inicio</a>
+                <p class="mb-4 text-dark">Te responderemos en la brevedad.</p>
+                <a class="btn btn-lg volverBtn mt-lg-4" href="/">Regresar al Inicio</a>
             </div>
         </div>
     </div>

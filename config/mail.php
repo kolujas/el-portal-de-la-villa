ca47f978b81e385a806755e8d9b27876b0af04dd
JuanCruzAGB <juancarmentia@gmail.com>
2020-01-14 18:48:01 -0300

    Test

--- config/mail.php
+++ config/mail.php
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME', 'info@elportaldelavilla.com.ar'),
+    'username' => env('MAIL_USERNAME'),
 
-    'password' => env('MAIL_PASSWORD', 'Elportaldelavilla2020'),
+    'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------

6a481140149fafe06f523bbf5611e52b3b1eba5c
JuanCruzAGB <juancarmentia@gmail.com>
2020-01-14 19:42:46 -0300

    Test

--- app/Http/Controllers/CorreoController.php
+++ app/Http/Controllers/CorreoController.php
@@ -6,6 +6,7 @@
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Mail;
     use Illuminate\Support\Facades\Validator;
+    use PHPMailer\PHPMailer\PHPMailer;
 
     class CorreoController extends Controller{
         /** 
